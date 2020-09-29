268df563daed4a3b33e0a23cb5669e1f739f40b2
MiguelUni <MiguelUni@DESKTOP-2SBFKDK>
2020-09-29 18:17:43 +0200

    segundo commit

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 1");
-		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
 
 	}
 
