3578d1d6414bb2ecb01c50d8a965b3996ad20091
MiguelUni <MiguelUni@DESKTOP-2SBFKDK>
2020-09-29 18:16:03 +0200

    cambios en fichero existente

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,7 +3,9 @@ package demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+		System.out.println("linea 1");
 
 	}
 
